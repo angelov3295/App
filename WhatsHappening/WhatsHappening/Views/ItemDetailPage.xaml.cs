a677f0d3c2629b825082efa3b8edc3f43c77a84d
angelo <angeloveldhuizen@hotmail.com>
2021-12-13 00:26:57 +0100

    Add project files.

---
+++ WhatsHappening/WhatsHappening/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using WhatsHappening.ViewModels;
+using Xamarin.Forms;
+
+namespace WhatsHappening.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
